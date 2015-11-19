88bf5d1dcb96721e1ebb91add770128777856f57
Jim Walker <jim.walker@namely.com>
2015-11-19 16:25:31 -0500

    cleanup arnoldb setup file

--- lib/arnoldb.rb
+++ lib/arnoldb.rb
@@ -6,14 +6,6 @@ require "arnoldb/proto/arnoldb-protofiles/ruby/arnoldb_services.rb"
 module Arnoldb
   autoload :Interface, "arnoldb/interface.rb"
   autoload :Connection, "arnoldb/connection.rb"
-  autoload :Mapper, "arnoldb/mapper.rb"
-  autoload :References, "arnoldb/generators/references.rb"
-  autoload :Migration, "arnoldb/migration.rb"
-  autoload :Schema, "arnoldb/schema.rb"
-
-  module Migrations
-    autoload :Generator, "arnoldb/migration/generator.rb"
-  end
 
   Arnoldb::Connection.connect
 end
