f44c8b6594809732dd23de9dfb3f8b3305ce6359
Jim Walker <jim.walker@namely.com>
2015-12-10 16:59:52 -0500

    change single quotes to double and add get_field test

--- spec/lib/interface_spec.rb
+++ spec/lib/interface_spec.rb
@@ -1,4 +1,4 @@
-require './spec/spec_helper.rb'
+require "./spec/spec_helper.rb"
 
 TYPES = {
   integer: 0,
@@ -14,21 +14,21 @@ COP = {
 }
 
 describe Arnoldb::Interface do
-  describe '.create_object_type' do
+  describe ".create_object_type" do
     let(:object_type_id) { Arnoldb::Interface.create_object_type("Profiles") }
     let(:empty) { Arnoldb::Interface.create_object_type("") }
 
-    it 'creates an object type in arnoldb' do
+    it "creates an object type in arnoldb" do
       expect(object_type_id).not_to eq(nil)
       expect(object_type_id).not_to eq("")
     end
 
-    it 'raises an error for empty title' do
+    it "raises an error for empty title" do
       expect { empty }.to raise_error(/Title required/)
     end
   end
 
-  describe '.create_field' do
+  describe ".create_field" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
     end
@@ -40,35 +40,35 @@ describe Arnoldb::Interface do
     let(:empty_title) { Arnoldb::Interface.create_field(@object_type_id, "", TYPES[:string]) }
     let(:wrong_value_type) { Arnoldb::Interface.create_field(@object_type_id, "value", 99) }
 
-    it 'creates a string field in arnoldb' do
+    it "creates a string field in arnoldb" do
       expect(string_field).not_to eq(nil)
       expect(string_field).not_to eq("")
     end
 
-    it 'creates an integer field in arnoldb' do
+    it "creates an integer field in arnoldb" do
       expect(integer_field).not_to eq(nil)
       expect(integer_field).not_to eq("")
     end
 
-    it 'creates a float field in arnoldb' do
+    it "creates a float field in arnoldb" do
       expect(float_field).not_to eq(nil)
       expect(float_field).not_to eq("")
     end
 
-    it 'raises an error for empty object type id' do
+    it "raises an error for empty object type id" do
       expect { empty_obj_type }.to raise_error(/Not a valid uuid/)
     end
 
-    it 'raises an error for empty title' do
+    it "raises an error for empty title" do
       expect { empty_title }.to raise_error(/Title required/)
     end
 
-    xit 'raises an error for wrong value type' do
+    xit "raises an error for wrong value type" do
       expect { wrong_value_type }.to raise_error(/Wrong type/)
     end
   end
 
-  describe '.create_object' do
+  describe ".create_object" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
     end
@@ -97,29 +97,29 @@ describe Arnoldb::Interface do
       )
     end
 
-    it 'creates an object in arnoldb' do
+    it "creates an object in arnoldb" do
       expect(object).not_to eq(nil)
       expect(object).not_to eq("")
     end
 
-    it 'creates an object in arnoldb with matching id' do
+    it "creates an object in arnoldb with matching id" do
       expect(object_with_id).to eq("b6785476-146d-43a4-a217-23e186ee7fd3")
     end
 
-    it 'raises an error for invalid object id' do
+    it "raises an error for invalid object id" do
       expect { invalid_obj_id }.to raise_error(/Not a valid uuid/)
     end
 
-    it 'raises an error for invalid object type id' do
+    it "raises an error for invalid object type id" do
       expect { invalid_obj_type_id }.to raise_error(/Not a valid uuid/)
     end
 
-    it 'raises an error for empty object type id' do
+    it "raises an error for empty object type id" do
       expect { empty_obj_type_id }.to raise_error(/Not a valid uuid/)
     end
   end
 
-  describe '.create_values' do
+  describe ".create_values" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
       @field_string = Arnoldb::Interface.create_field(@object_type_id, "name", TYPES[:string])
@@ -231,7 +231,7 @@ describe Arnoldb::Interface do
       Arnoldb::Interface.create_values(empty_field_id)
     end
 
-    it 'creates current values in arnoldb' do
+    it "creates current values in arnoldb" do
       expected = []
       value_set1.each do |value|
         expected << { id: value[:object_id], value: value[:value] }
@@ -240,7 +240,7 @@ describe Arnoldb::Interface do
       expect(current_values).to match_array(expected)
     end
 
-    it 'creates past values in arnoldb' do
+    it "creates past values in arnoldb" do
       expected = []
       value_set2.each do |value|
         expected << { id: value[:object_id], value: value[:value] }
@@ -249,7 +249,7 @@ describe Arnoldb::Interface do
       expect(past_values).to match_array(expected)
     end
 
-    it 'creates future values in arnoldb' do
+    it "creates future values in arnoldb" do
       expected = []
       value_set3.each do |value|
         expected << { id: value[:object_id], value: value[:value] }
@@ -258,39 +258,39 @@ describe Arnoldb::Interface do
       expect(future_values).to match_array(expected)
     end
 
-    it 'raises error for object id' do
+    it "raises error for object id" do
       expect { bad_obj_id }.to raise_error(/Object Id Not Found/)
     end
 
-    it 'raises error for object type id' do
+    it "raises error for object type id" do
       expect { bad_obj_type_id }.to raise_error(/Field not associated with given Object Type/)
     end
 
-    it 'raises error for field id' do
+    it "raises error for field id" do
       expect { bad_field_id }.to raise_error(/Field Not Found/)
     end
   end
 
   # TODO NEED TO FIGURE OUT HOW GET_OBJECT_TYPE SHOULD FUNCTION
-  describe '.get_object_type' do
+  describe ".get_object_type" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
     end
 
-    xit 'gets an object type from arnoldb' do
+    xit "gets an object type from arnoldb" do
       result = Arnoldb::Interface.get_object_type("Profiles")
 
       expect(result).to eq(@object_type_id)
     end
 
-    xit 'gets an object type from arnoldb' do
+    xit "gets an object type from arnoldb" do
       result = Arnoldb::Interface.get_object_type("")
 
       expect(result).to eq("")
     end
   end
 
-  describe '.get_object_types' do
+  describe ".get_object_types" do
      before(:all) do
        @object_type_ids = [
            Arnoldb::Interface.create_object_type("Profiles"),
@@ -299,14 +299,33 @@ describe Arnoldb::Interface do
        ]
     end
 
-    it 'gets all object types from arnoldb' do
+    it "gets all object types from arnoldb" do
       result = Arnoldb::Interface.get_all_object_types
 
       expect(result).to include(*@object_types_ids)
     end
   end
 
-  describe '.get_fields' do
+  describe ".get_field" do
+    before(:all) do
+      @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
+      @field_string = Arnoldb::Interface.create_field(@object_type_id, "first_name", TYPES[:string])
+    end
+
+    it "gets field from Arnoldb" do
+      expected = {
+        id: @field_string,
+        object_type_id: @object_type_id,
+        title: "first_name",
+        value_type: :STRING
+      }
+      result = Arnoldb::Interface.get_field(@field_string)
+
+      expect(result).to match(expected)
+    end
+  end
+
+  describe ".get_fields" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
       @field_string = Arnoldb::Interface.create_field(@object_type_id, "name", TYPES[:string])
@@ -323,18 +342,18 @@ describe Arnoldb::Interface do
     end
     let(:bad_obj_type_id) { Arnoldb::Interface.get_fields("") }
 
-    it 'gets fields from arnoldb' do
+    it "gets fields from arnoldb" do
       result = Arnoldb::Interface.get_fields(@object_type_id)
 
       expect(result).to match_array(fields)
     end
 
-    it 'raises an error if bad object_type_id' do
+    it "raises an error if bad object_type_id" do
       expect { bad_obj_type_id }.to raise_error(/Not a valid uuid/)
     end
   end
 
-  describe '.get_values' do
+  describe ".get_values" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
       @field_string = Arnoldb::Interface.create_field(@object_type_id, "name", TYPES[:string])
@@ -453,7 +472,7 @@ describe Arnoldb::Interface do
       )
     end
 
-    it 'gets current values from arnoldb' do
+    it "gets current values from arnoldb" do
       expected = []
       value_set1.each do |value|
         expected << { id: value[:object_id], value: value[:value] }
@@ -463,7 +482,7 @@ describe Arnoldb::Interface do
       expect(current_values).to match_array(expected)
     end
 
-    it 'gets past values from arnoldb' do
+    it "gets past values from arnoldb" do
       expected = []
       value_set2.each do |value|
         expected << { id: value[:object_id], value: value[:value] }
@@ -476,7 +495,7 @@ describe Arnoldb::Interface do
       expect(past_values).to match_array(expected)
     end
 
-    it 'gets future values from arnoldb' do
+    it "gets future values from arnoldb" do
       expected = []
       value_set3.each do |value|
         expected << { id: value[:object_id], value: value[:value] }
@@ -489,24 +508,24 @@ describe Arnoldb::Interface do
       expect(future_values).to match_array(expected)
     end
 
-    it 'raises an error if bad object_type_id' do
+    it "raises an error if bad object_type_id" do
       expect { empty_object_type_id }.to raise_error(/Object Type Not Found/)
     end
 
-    it 'raises an error if bad object_id' do
+    it "raises an error if bad object_id" do
       expect { empty_object_id }.to raise_error(/Not a valid uuid/)
     end
 
-    it 'raises an error if bad field_id' do
+    it "raises an error if bad field_id" do
       expect { empty_field_id }.to raise_error(/Field Not Found/)
     end
 
-    it 'raises an error if one bad object_id' do
+    it "raises an error if one bad object_id" do
       expect { one_empty_object_id }.to raise_error(/Not a valid uuid/)
     end
   end
 
-  describe '.get_objects' do
+  describe ".get_objects" do
     before(:all) do
       @object_type_id = Arnoldb::Interface.create_object_type("Profiles")
       @field_string = Arnoldb::Interface.create_field(@object_type_id, "name", TYPES[:string])
@@ -612,7 +631,7 @@ describe Arnoldb::Interface do
       )
     end
 
-    xit 'gets objects from arnoldb' do
+    xit "gets objects from arnoldb" do
       Arnoldb::Interface.create_values(value_set1)
 
       result = Arnoldb::Interface.get_objects(
@@ -635,19 +654,19 @@ describe Arnoldb::Interface do
       expect(result).to include(@obj_1)
     end
 
-    it 'raises an error with empty object_type_id' do
+    it "raises an error with empty object_type_id" do
       Arnoldb::Interface.create_values(value_set1)
 
       expect { bad_obj_type_id }.to raise_error(/Not a valid uuid/)
     end
 
-    it 'raises an error with empty field_id' do
+    it "raises an error with empty field_id" do
       Arnoldb::Interface.create_values(value_set1)
 
       expect { bad_field_id }.to raise_error(/Not a valid uuid/)
     end
 
-    it 'raises an error with empty operator' do
+    it "raises an error with empty operator" do
       Arnoldb::Interface.create_values(value_set1)
 
       expect { bad_operator }.to raise_error(/Not a valid operator/)
