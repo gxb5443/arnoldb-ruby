3d66651b8eb65b5f4975177f474a170b3dafda91
Attila Maczak <attila@maczak.hu>
2015-12-17 15:21:05 +0100

    typo

--- lib/arnoldb.rb
+++ lib/arnoldb.rb
@@ -7,7 +7,7 @@ require "arnoldb/connection.rb"
 module Arnoldb
   autoload :Interface, "arnoldb/interface.rb"
 
-  def connection(@arnoldb_address)
-    Proto::DatastoreActions::Stub.new(@arnoldb_address)
+  def connection(arnoldb_address)
+    Proto::DatastoreActions::Stub.new(arnoldb_address)
   end
 end
